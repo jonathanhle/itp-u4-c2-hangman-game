f71cd37261cbc1389ff353d279256c177e4384ef
Jonathan Le <jonathan.hle@gmail.com>
2018-07-05 12:02:49 +0000

    Added/read through guess_letter solution

--- hangman/game.py
+++ hangman/game.py
@@ -44,19 +44,46 @@ def _uncover_word(answer_word, masked_word, character):
             new_masked_word += '*'
             
     return new_masked_word
-        
-        
-    
+
+def _is_game_won(game):
+    return game['answer_word'].lower() == game['masked_word'].lower()
+
+def _is_game_lost(game):
+    return game['remaining_misses'] <= 0
+
+
+def _is_game_finished(game):
+    return _is_game_lost(game) or _is_game_won(game)
+
 
 def guess_letter(game, letter):
-    pass
+    letter = letter.lower()
+    if letter in game['previous_guesses']:
+        raise InvalidGuessedLetterException()
+
+    if _is_game_finished(game):  # Already Won
+        raise GameFinishedException()
+
+    previous_masked = game['masked_word']
+    new_masked = _uncover_word(game['answer_word'], previous_masked, letter)
+
+    if previous_masked == new_masked:
+        # This is a miss!
+        game['remaining_misses'] -= 1
+    else:
+        # This is a correct guess!
+        game['masked_word'] = new_masked
+
+    game['previous_guesses'].append(letter)
+
+    if _is_game_won(game):
+        raise GameWonException()
+
+    if _is_game_lost(game):
+        raise GameLostException()
+
+    # return new_masked
     
-
-
-
-
-
-
 
 def start_new_game(list_of_words=None, number_of_guesses=5):
     if list_of_words is None:
